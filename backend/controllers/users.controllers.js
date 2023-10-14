f70e528a31b73e9e2e6167021bc82fdf5bde7fd6
Nicolás Bobb <nicobobb@gmail.com>
2023-10-14 18:44:32 -0300

    ADD users controllers

--- backend/controllers/users.controllers.js
+++ backend/controllers/users.controllers.js
@@ -1,4 +1,21 @@
+import UserModel from "../models/users.models.js";
+
 const login = async (req, res) => {
-    res.json({ message: "Error al buscar el usuario" });
+    const newUser = await new UserModel({
+        firstname: "Pepe",
+        lastname: "Pechudo",
+        email: "elpepe56.pechudo@gmail.com",
+        roles: {
+            admin: true,
+            doctor: false,
+        },
+    });
+
+    // Guarda el nuevo usuario en la base de datos
+    await newUser.save();
+
+    console.log("Usuario guardado exitosamente:", newUser);
+    res.json({ message: newUser });
 };
+
 export { login };
--- backend/database/connect.js
+++ backend/database/connect.js
@@ -1,47 +1,14 @@
 import mongoose from "mongoose";
-import "dotenv/config.js";
-
-// Define un esquema de usuario
-const userSchema = new mongoose.Schema({
-    firstname: String,
-    lastname: String,
-    email: { type: String, unique: true },
-    roles: {
-        admin: Boolean,
-        doctor: Boolean,
-    },
-});
-
-// Crea un modelo de usuario a partir del esquema
-const User = mongoose.model("User", userSchema);
 
 async function connectDB() {
     if (!process.env.MONGODB_URL) {
         throw new Error("Falta la variable de entorno MONGODB_URL");
     }
     try {
-        await mongoose.connect(process.env.MONGODB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("Conexión exitosa con MongoDB");
-
-        const newUser = new User({
-            firstname: "Pepe",
-            lastname: "Pechudo",
-            email: "elpepe2.pechudo@gmail.com",
-            roles: {
-                admin: true,
-                doctor: false,
-            },
-        });
-
-        // Guarda el nuevo usuario en la base de datos
-        await newUser.save();
-
-        console.log("Usuario guardado exitosamente:", newUser);
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("Conexión exitosa con Mongo DB");
     } catch (error) {
-        console.log("Hubo un error al conectarnos a la Base de Datos", error);
+        console.log("Hubo un error al conectarnos a la BBDD", error);
     }
 }
 
