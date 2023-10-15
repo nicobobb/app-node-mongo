e7896fc330fbd41a0608303aadb654689ecc374f
Nicolás Bobb <nicobobb@gmail.com>
2023-10-15 11:56:11 -0300

    FIX eslint module export and import

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config.js";
-import routerApi from "./routes/index.js";
-import connectDB from "./database/connect.js";
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config.js';
+import {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} from './middlewares/error.handler.js';
+import routerApi from './routes/index.js';
+import connectDB from './database/connect.js';
 
 const app = express();
 connectDB();
@@ -12,8 +17,12 @@ app.use(cors());
 
 routerApi(app);
 
-app.use("/status", (_, res) => {
-    res.status(200).send(process.env.FRONTEND_URL);
+app.use('/status', (_, res) => {
+  res.status(200).send(process.env.FRONTEND_URL);
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(5000, () => console.log('Server running on port 5000'));
