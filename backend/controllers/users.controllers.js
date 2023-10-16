3b23cb36413ecacc76e21fe0bf8e521f406093b3
Nicolás Bobb <nicobobb@gmail.com>
2023-10-16 16:22:28 -0300

    Update users.controllers.js

--- backend/controllers/users.controllers.js
+++ backend/controllers/users.controllers.js
@@ -1,29 +1,30 @@
-import UserModel from "../models/users.models.js";
+import UserModel from '../models/users.models.js';
 
-const createUser = async (req, res) => {
+const createUser = async (req, res, next) => {
+  try {
     const { firstname, lastname, email, roles, pass } = req.body;
     const newUser = await new UserModel({
-        firstname,
-        lastname,
-        email,
-        roles,
-        pass,
+      firstname,
+      lastname,
+      email,
+      roles,
+      pass,
     }).save();
 
-    console.log("Usuario guardado exitosamente:", newUser);
+    console.log('Usuario guardado exitosamente:', newUser);
     res.json({ message: newUser });
+  } catch (error) {
+    next(error);
+  }
 };
 
-const getAllUsers = async (req, res) => {
-    try {
-        const allUsers = await UserModel.find();
-        res.json(allUsers);
-    } catch (error) {
-        console.error("Error al obtener todos los usuarios:", error);
-        res.status(500).json({
-            error: "Hubo un error al obtener todos los usuarios",
-        });
-    }
+const getAllUsers = async (req, res, next) => {
+  try {
+    const allUsers = await UserModel.find();
+    res.json(allUsers);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export { createUser, getAllUsers };
