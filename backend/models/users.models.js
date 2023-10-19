8fd993ed19d40fe2df1ab33eeb078ea36b8610aa
Nicolás Bobb <nicobobb@gmail.com>
2023-10-19 10:11:58 -0300

    Prettier format

--- backend/models/users.models.js
+++ backend/models/users.models.js
@@ -1,17 +1,17 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from 'mongoose'
 
 const userSchema = new Schema({
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  pass: { type: String, required: true },
-  roles: {
-    type: {
-      admin: Boolean,
-      doctor: Boolean,
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    pass: { type: String, required: true },
+    roles: {
+        type: {
+            admin: Boolean,
+            doctor: Boolean,
+        },
+        required: true,
     },
-    required: true,
-  },
-});
+})
 
-export default model('User', userSchema, 'users');
+export default model('User', userSchema, 'users')
