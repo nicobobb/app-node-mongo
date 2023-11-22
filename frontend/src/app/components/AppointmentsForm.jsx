727cff9dfdc8e4be825f2206e57d6a8d351b7866
Nico Bobb <nicobobb@gmail.com>
2023-11-22 16:11:00 -0300

    Add AppointmentsForm component and update home page layout

---
+++ frontend/src/app/components/AppointmentsForm.jsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import toast, { Toaster } from "react-hot-toast";
+
+const AppointmentsForm = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = async (data) => {
+        console.log(data);
+    };
+
+    return (
+        <>
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="max-w-md mx-auto mt-4"
+            >
+                <div className="mb-4">
+                    <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="title"
+                    >
+                        Título:
+                    </label>
+                    <input
+                        type="text"
+                        id="title"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        placeholder="Título de la cita"
+                        {...register("title", { required: true })}
+                    />
+                </div>
+                <div className="mb-4">
+                    <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="start"
+                    >
+                        Inicio:
+                    </label>
+                    <input
+                        type="datetime-local"
+                        id="start"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        {...register("start", { required: true })}
+                    />
+                </div>
+                <div className="mb-4">
+                    <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="end"
+                    >
+                        Fin:
+                    </label>
+                    <input
+                        type="datetime-local"
+                        id="end"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        {...register("end", { required: true })}
+                    />
+                </div>
+                <div className="mb-4">
+                    <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="userId"
+                    >
+                        ID de Usuario:
+                    </label>
+                    <input
+                        type="text"
+                        id="userId"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        placeholder="ID de usuario"
+                        {...register("useId", { required: true })}
+                    />
+                </div>
+                <div className="flex items-center justify-between">
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit"
+                    >
+                        Guardar
+                    </button>
+                </div>
+            </form>
+            <Toaster />
+        </>
+    );
+};
+
+export { AppointmentsForm };
--- frontend/src/app/home/page.js
+++ frontend/src/app/home/page.js
@@ -1,8 +1,12 @@
+import { AppointmentsForm } from "@/app/components/AppointmentsForm";
+
 const home = () => {
     return (
         <>
-            <h1>Panel de Administración</h1>
-            <p>¡Bienvenido!</p>
+            <h1 className="text-center mt-12 text-2xl">
+                Panel de Administración
+            </h1>
+            <AppointmentsForm />
         </>
     );
 };
